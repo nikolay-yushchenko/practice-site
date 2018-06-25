4f8673ed2a9774f2e11751d3bccbedf0f2e758e3
nikolay-yushchenko <30828083+nikolay-yushchenko@users.noreply.github.com>
2018-06-26 02:32:49 +0300

    practice-site

--- php/log.php
+++ php/log.php
@@ -1,33 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nik
- * Date: 24.06.2018
- * Time: 21:06
- */
 require_once 'DB.php';
-//$logname=$_POST['name'];
-//$logpass=$_POST['password'];
-
-$logname="tvcore";
-$logpass="12345";
-
+$nim=$_POST['name'];
+$nimp=$_POST['password'];
 $result = mysqli_query(DataBase::Connect(), "SELECT * FROM `logreg` ");
+
 if ($result) {
     while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
-        echo "ID:".$row[0]." ","Name:".$row[1]." ","Password:".$row[2]."<br>";
-        if (preg_match("/\b$logname\b/",$row[1]) && preg_match("/\b$logpass\b/", $row[2])) {
-            //echo json_encode("True");
-            print("Yessssssss");
-            //break;
+      if (preg_match("/\b$nim\b/",$row[1]) && preg_match("/\b$nimp\b/",$row[2])) {
+            $rey="True";
+           break;
         } else {
-            //echo json_encode("False");
-            print("Nooooooooo");
-
+            $rey="False";
         }
-    }
-    mysqli_free_result($result);
-    DataBase::Close();
+       
+        
 }
+echo json_encode($rey); 
+}
+DataBase::Close();
 
 ?>

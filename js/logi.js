cea79d2a52b611f03fa3bba04082d3674ebc9982
nikolay-yushchenko <30828083+nikolay-yushchenko@users.noreply.github.com>
2018-06-27 02:55:42 +0300

    practice-site

--- js/logi.js
+++ js/logi.js
@@ -1,4 +1,4 @@
-function valid(form){
+        function valid(form){
     if(form.name.value && form.password.value)call('#formx','../php/log.php');
 }
     
@@ -14,4 +14,7 @@ function call(formtype,filex){
 }
 function success(result){
 alert("ok");
+        var result=$.parseJSON(result);
+    if(result==="True")window.location="admin.html";
+    else alert("Проверьте правильность данных!");
 }

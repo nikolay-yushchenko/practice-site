b72a73e78c5c2f9dad0ba1136801e3b279023c1c
nikolay-yushchenko <30828083+nikolay-yushchenko@users.noreply.github.com>
2018-06-27 01:22:12 +0300

    practice-site

--- php/upload.php
+++ php/upload.php
@@ -5,18 +5,11 @@
  * Date: 26.06.2018
  * Time: 22:50
  */
+header( "refresh:10;url=../admin.html" );
 $name= $_POST["name"];
 $price= $_POST["price"];
 $desc= $_POST["description"];
 $n= $_POST["quantity"];
-echo $_POST["name"];
-echo $_POST["price"];
-echo $_POST["description"];
-echo $_POST["quantity"];
-echo "<p><b>Оригинальное имя загруженного файла: ".$_FILES['uploadfile']['name']."</b></p>";
-echo "<p><b>Mime-тип загруженного файла: ".$_FILES['uploadfile']['type']."</b></p>";
-echo "<p><b>Размер загруженного файла в байтах: ".$_FILES['uploadfile']['size']."</b></p>";
-echo "<p><b>Временное имя файла: ".$_FILES['uploadfile']['tmp_name']."</b></p>";
 
 mkdir("../img/$name", 0700);
 $uploaddir = "../img/$name/";
@@ -27,12 +20,20 @@ $uploadfilet = $uploaddir.basename($_FILES['uploadfilet']['name']);
 $upload=$uploadfile.$uploadfiles.$uploadfilet;
 if (move_uploaded_file($_FILES['uploadfile']['tmp_name'], $uploadfile)&&move_uploaded_file($_FILES['uploadfiles']['tmp_name'], $uploadfiles) &&move_uploaded_file($_FILES['uploadfilet']['tmp_name'], $uploadfilet))
 {
-    echo "<h3>Файлы успешно загружен на сервер</h3>";
+    echo "<h3>Файлы успешно загружены на сервер</h3>";
 }
 else { echo "<h3>Ошибка! Не удалось загрузить файлы на сервер!</h3>"; exit; }
 
 require_once 'DB.php';
 $gorder="INSERT INTO `goods`(`idgood`, `name`, `price`, `description`, `image`, `quantity`) VALUES (NULL,'$name',$price,'$desc','$upload',$n)";
 mysqli_query(DataBase::Connect(), $gorder);
+
+echo "<p><b>Название товара:".$_POST["name"]."</b></p>";
+echo "<p><b>Цена товара: ".$_POST["price"]."</b></p>";
+echo "<p><b>Описание товара: ".$_POST["description"]."</b></p>";
+echo "<p><b>Количество на складе: ".$_POST["quantity"]."</b></p>";
+echo "<img style='border: 1px solid red' src='$uploadfile' />";
+echo "<img  style='border: 1px solid red'  src='$uploadfiles' />";
+echo "<img  style='border: 1px solid red' src='$uploadfilet' />";
 
 ?>
